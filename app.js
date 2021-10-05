6e21891fe73929578b30f233b9e23144c2e05df9
smilingfroggy <smilingfroggy@hotmail.com>
2021-10-05 18:31:28 +0800

    feat: add template engine - handlebars

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
+// template engine
+app.engine('handlebars', exphbs({ defaultLayout: "main" }))
+app.set('view engine', 'handlebars')
+// static files: style.css
+app.set(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send(`<h3>URL Shortener</h3>
-<img src="https://assets-lighthouse.alphacamp.co/uploads/image/file/16716/ExportedContentImage_00.png" alt="">`)
+  //   res.send(`<h3>URL Shortener</h3>
+  // <img src="https://assets-lighthouse.alphacamp.co/uploads/image/file/16716/ExportedContentImage_00.png" alt="">`)
+  res.render('index')
 })
 
 app.listen(PORT, () => {
